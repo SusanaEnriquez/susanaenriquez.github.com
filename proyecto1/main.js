299363a9f219127e17ca80097f1699d215dc16bb
SusanaEnriquez <55167603+SusanaEnriquez@users.noreply.github.com>
2021-06-06 23:02:53 -0500

    Proyecto

---
+++ proyecto1/main.js
@@ -0,0 +1,49 @@
+// DOM
+
+/* querySelector | querySelectorAll
+// let links = document.querySelectorAll("a");
+
+inks.forEach(function(link) {
+    console.log(link);
+});*/
+
+/*let celdas = document.querySelectorAll("td");
+celdas.forEach(function (td) {
+    td.addEventListener('click',function () {
+        console.log(this)
+    })
+    
+})*/
+
+/* Eliminar el evento por default de algo */
+let links = document.querySelectorAll('.close');
+links.forEach(function(link){
+    link.addEventListener('click',function(evento){
+        evento.preventDefault();
+
+        let content = document.querySelector('.content');
+
+        // Quitarle las clases de animacion
+        content.classList.remove('animate__animated animate__fadeInDown')
+
+        // Agregarle clases de animacion
+        content.classList.add('animate__animated animate__fadeOutUp')
+
+        // Retrasar el redireccionamiento
+        setTimeout(function(){
+          location.href = '/';  
+        },600); // una sola vez
+
+        // setInterval -> constantemente
+        
+        return false;
+    });
+}); 
+
+/* Eliminar o agregar elementos
+let iconos = document.querySelectorAll('i');
+iconos.forEach(function(icono){
+    icono.classList.remove('fa-plane-departure');
+    icono.classList.add('fa-plane-departure');
+}); */
+
